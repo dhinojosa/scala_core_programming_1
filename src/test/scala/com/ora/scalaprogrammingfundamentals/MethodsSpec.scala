987d23e32303acddc67d6a3f010cc4d6c3d66914
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2018-03-24 12:47:24 -0600

    cleaned up files, screwed up setup

---
+++ src/test/scala/com/ora/scalaprogrammingfundamentals/MethodsSpec.scala
@@ -0,0 +1,19 @@
+package com.ora.scalaprogrammingfundamentals
+
+import org.scalatest.{FunSuite, Matchers}
+
+class MethodsSpec extends FunSuite with Matchers{
+   test("In review, a method is structured like the following:") {
+     def foo(x:Int, y:Int):Int = {
+         x + y
+     }
+     foo(4, 5) should be (9)
+   }
+
+  test("Also a method can be inlined if there is only one statement:") {
+    def foo(x:Int, y:Int):Int = x + y
+    foo(4, 5) should be (9)
+  }
+
+  test("")
+}
